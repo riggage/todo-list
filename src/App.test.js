b0149bb440d5c2849ceb41cf86d26ea3709bf567
riggage <riggage@hotmail.co.uk>
2022-02-14 10:45:42 +0000

    second commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import { render, screen } from '@testing-library/react';
-// import App from './App';
+import App from './App';
 import Form from './components/Form'
 
-test('renders heading', () => {
-  render(<Form />);
-  const element = screen.getAllByRole("button")
+test('renders header', () => {
+  render(<App />);
+  const element = screen.getByRole("textbox");
   expect(element).toBeInTheDocument();
 });
+
+
+test('add button is in the document', () => {
+  render(<Form />)
+  const button = screen.getByText("add")
+  expect(button).toBeInTheDocument()
+})
+
+test("input element is in the document", () => {
+  render(<App />)
+  const input = screen.getByPlaceholderText("Add an item...")
+  expect(input).toBeInTheDocument()
+})
